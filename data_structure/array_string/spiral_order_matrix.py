c11285b6afc1f8163b2b1f9a0628f4a2c37c077a
mir10 <mirmahfuz10@gmail.com>
2023-09-08 21:23:11 +0600

    working on spiral order matrix

---
+++ data_structure/array_string/spiral_order_matrix.py
@@ -0,0 +1,42 @@
+#Sprial Matrix
+
+#Given an m x n matrix ,return all elements of the matrix in spiral order.
+#Input: matrix = [[1,2,3], [4,5,6], [7,8,9]]
+
+# [
+#     [1,2,3], 
+#     [4,5,6], 
+#     [7,8,9]
+
+#     ]
+
+#Output: [1,2,3,6,9,8,7,4,5]
+
+
+
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            #get every i in top row
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1   
+
+            #get every i in the right col
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+
+
+
+
+
+
+        
+
+
+# matrix = [ [1,2,3], [4,5,6], [7,8,9]]
+# Solution().spiralOrder(matrix)   
