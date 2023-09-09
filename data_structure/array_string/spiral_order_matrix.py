5c8de2f12612c7ce605414a5dd3b2cda8584d185
mir10 <mirmahfuz10@gmail.com>
2023-09-09 08:54:39 +0600

    spiral matrix implemented

--- data_structure/array_string/spiral_order_matrix.py
+++ data_structure/array_string/spiral_order_matrix.py
@@ -13,9 +13,8 @@
 #Output: [1,2,3,6,9,8,7,4,5]
 
 
-
 class Solution:
-    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+    def spiralOrder(self, matrix):
         res = []
         left, right = 0, len(matrix[0])
         top, bottom = 0, len(matrix)
@@ -29,14 +28,30 @@ class Solution:
             #get every i in the right col
             for i in range(top, bottom):
                 res.append(matrix[i][right - 1])
+            right -= 1 
+
+            if not (left < right and top < bottom):
+                break
+
+            #get every i in bottom row
+            # print(right)
+            print(left)
+
+
+            print(matrix[2][1])
+
+
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom -1][i])
+            bottom -= 1
+
+            #get every i in the left col
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+                left += 1
+        return res        
 
 
 
-
-
-
-        
-
-
-# matrix = [ [1,2,3], [4,5,6], [7,8,9]]
-# Solution().spiralOrder(matrix)   
+matrix = [ [1,2,3], [4,5,6], [7,8,9]]
+print(Solution().spiralOrder(matrix))   
