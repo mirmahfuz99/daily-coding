283681443f98b04df9ffb7f1b43669e43ccdeab3
mir10 <mirmahfuz10@gmail.com>
2023-09-21 00:31:09 +0600

    merge intervals implemented

---
+++ data_structure/array_string/merge_intervals.py
@@ -0,0 +1,16 @@
+def insert(intervals, new_interval):
+    res = []
+    for i in range(len(intervals)):
+        if new_interval[1] < intervals[i][0]:
+            res.append(new_interval)
+            return res + intervals[i:]
+
+        elif new_interval[0] > intervals[i][1]:
+            res.append(intervals[i])
+        else:
+            new_interval = [min(new_interval[0], intervals[i][0]), max(new_interval[1], intervals[i][1])]
+
+    res.append(new_interval)
+    return res           
+
+print(insert([[1, 3], [6,9]], [2, 5]))   
